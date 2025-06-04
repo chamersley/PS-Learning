e551e1f5a11731d0258f5f72d3c616905d009efb
chamersley <99451841+chamersley@users.noreply.github.com>
2025-06-04 13:37:05 -0400

    Added Functions.ps1

---
+++ Functions.ps1
@@ -0,0 +1,32 @@
+﻿Set-StrictMode -Version Latest
+
+function Create-Configuration{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string] $Name,
+    
+        [Parameter()]
+        [ValidateSet("1","2")]
+        [string] $Version="1"
+        
+    )
+
+    begin{
+        "Creating Configuration file with the name"
+    }
+
+    process{
+        Write-Output "Creating Configuration file with the name : $Name at version : $Version ..."
+    }
+
+    end{
+        "Creating Configuration file with the name"
+    }
+
+}
+
+$Names=@("Test1", "Test2", "Test3")
+
+$Names | Create-Configuration -Version "2"
+
