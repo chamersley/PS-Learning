038c872718d6e8ac1707bbb1bc666f67fe206239
chamersley <99451841+chamersley@users.noreply.github.com>
2025-06-05 13:44:39 -0400

    Added Remoting and support file GetHostName

---
+++ Remoting.ps1
@@ -0,0 +1,39 @@
+﻿hostname
+
+$TestBlock={ Write-Host "This is a script-block" }
+
+& $TestBlock
+
+Invoke-Command -ComputerName AAA2KC2J64 -FilePath C:\Users\chamersley\Documents\GetHostName.ps1
+
+Invoke-Command -ComputerName AAA2KC2J64 -ScriptBlock { Write-Host "This is running on $(hostname)" }
+
+$SomeVariable="This is a local variable"
+
+
+Invoke-Command -ComputerName AAA2KC2J64 -ScriptBlock { 
+    Write-Host "This is running on $(hostname) and $($args[0])" } -ArgumentList $SomeVariable
+
+Invoke-Command -ComputerName AAA2KC2J64 -ScriptBlock { 
+    Write-Host "This is running on $(hostname) and $($args[0])" }
+
+Invoke-Command -ComputerName AAA2KC2J64 -ScriptBlock { 
+    $test="This is running on $(hostname) and $($args[0])" Write-Host $test }
+
+New-PSSession -ComputerName AAA2KC2J64 
+
+$Session=Get-PSSession -ComputerName AAA2KC2J64
+
+$Session
+
+Invoke-Command -Session $Session -Script-Block {
+    $test="This is running on $(hostname) and $($args[0])" Write-Host $test 
+}
+
+Invoke-Command -Session $Session -ScriptBlock {
+    Write-Host $test
+}
+
+Disconnect-PSSession -Session $Session
+
+Get-PSSession | Disconnect-PSSession | Remove-PSSession
