39f5652c46289d46b048ffd5864405c49a1e695d
chamersley <99451841+chamersley@users.noreply.github.com>
2025-06-05 12:31:51 -0400

    Add files via upload

---
+++ Modules.ps1
@@ -0,0 +1,28 @@
+﻿
+$env:PSModulePath
+
+#Get-Module -ListAvailable
+
+Import-Module -Name WindowsUpdate
+
+Get-Command -Module WindowsUpdate
+
+#Get-WindowsUpdateLog
+
+Import-Module -name WindowsUpdate -Force
+
+Remove-Module -Name WindowsUpdate
+
+New-ModuleManifest -Path "C:\Users\chamersley\Documents\WindowsPowerShell\Modules\Configurations\Configurations.psd1" -Description "This is the configurations module" -RootModule Configurations.psm1
+
+Get-Module -Name Configurations -ListAvailable
+
+Get-Configuration
+
+Remove-Module -Name Configurations
+
+Find-Module -Name AzureAD
+
+Import-Module -Name AzureAD
+
+#Connect-AzureAD
