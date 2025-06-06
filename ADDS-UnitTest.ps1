380ff5ce4ddbb23a7d9659a9fe85fe637e10b38a
chamersley <99451841+chamersley@users.noreply.github.com>
2025-06-06 11:02:27 -0400

    Pester Section Files
    AADS and Pester Files

---
+++ ADDS-UnitTest.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    $env:COMPUTERNAME
+)
+describe 'Check Server' {
+    context 'Checking Windows Features'{
+        it 'Making sure AD Domain Services is installed'{
+            $DomainServices=Invoke-Command -Computername $ComputerName -scriptblock{
+                Get-WindowsFeature -Name AD-Domain-Services
+            }
+        }
+            $DomanServices.Installed | should be $true
+    }
+    context 'Checking Windows Services'{
+        it 'Making Sure Windows Update Service is running'{
+            $UpdateService=Invoke-Command -ComputerName $env:COMPUTERNAME -scriptblock{
+                Get-Service -DisplayName 'Windows Update'
+            }
+            $UpdateService.status | should be "Running"
+        }
+    }
+}
---
+++ Pester.ps1
@@ -0,0 +1,12 @@
+﻿Invoke-Pester -Script (.'C:\users\chamersley\documents\ADDS-UnitTest.ps1' -ComputerName ihwanglpt1)
+
+$ScriptParameters=@{
+path='C:\users\chamersley\documents\ADDS-UnitTest.ps1'
+Parameters=@{
+    ComputerName='ihwanglpt1'
+}
+}
+
+$ScriptParameters
+
+Invoke-Pester -Script $ScriptParameters -OutputFile c:\youtube.xml -OutputFormat NUnitXml
