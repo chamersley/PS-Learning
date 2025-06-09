f100e51e62f465faf555a10834e7a5bf7b193a72
chamersley <99451841+chamersley@users.noreply.github.com>
2025-06-09 12:45:24 -0400

    Importing CSV Files

---
+++ CSV.ps1
@@ -0,0 +1,11 @@
+﻿$CSVFilePath="D:\DATA\Employee1.csv"
+
+Get-Content -Path $CSVFilePath
+
+$EmployeeData=Import-CSV -Path $CSVFilePath -Delimiter ','
+
+foreach($Employee in $EmployeeData){
+    Write-Host "$($Employee.FirstName) $($Employee.LastName) has date of birth of $($Employee.DOB)"
+}
+
+Get-Service | Select-Object -Property Status,Name,DisplayName,@{Name="Time";Expression={Get-Date -Format 'MM-dd-yy hh:mm:ss'}} | Export-CSV -Path G: -Delimiter ',' -NoTypeInformation
